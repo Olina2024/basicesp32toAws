06821ef2b7562083658430c4f729aaae828d824f
Jing Ouyang <ouyangjing1983@gmail.com>
2025-12-09 18:57:56 +0100

    Add GPIO.h with sensor definitions and thresholds

---
+++ GPIO.h
@@ -0,0 +1,9 @@
+#define SDA 4
+#define SCL 5
+#define LM75_ADDR 0x48
+#define Anlog_Pin 9
+float temperature;
+float humidity;
+
+float temperature_threshold = 20.0;
+float humidity_threshold = 70.0;
