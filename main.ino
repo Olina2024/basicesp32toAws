678d3e18a15e235f689343b124d6324fdd142484
Jing Ouyang <ouyangjing1983@gmail.com>
2025-12-21 19:49:33 +0100

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,24 +1,73 @@
-// Include required libraries for WiFi, secure connections, MQTT, and JSON parsing
-#include "secrets.h"
+#define BLYNK_TEMPLATE_ID "TMPL4VQ2tDL3T"
+#define BLYNK_TEMPLATE_NAME "Esp32"
+#define BLYNK_AUTH_TOKEN "jXpx3L5GRNQe48L_8S8BaRx-ajNrDQ9G"
+#include <InfluxDbClient.h>
+#include <InfluxDbCloud.h>
+#include <Temperature_LM75_Derived.h>
+#include "secret.h"
 #include <WiFiClientSecure.h>
-#include <MQTTClient.h>
 #include <ArduinoJson.h>
 #include "WiFi.h"
 #include "Wire.h"
-#include "GPIO.h"
-// The MQTT topics that this device should publish/subscribe
-#define AWS_IOT_PUBLISH_TOPIC "/telemetry"      // Topic for publishing sensor data
-#define AWS_IOT_SUBSCRIBE_TOPIC "/downlink"     // Topic for receiving commands from AWS
+#include "gpio.h"
+#include <BlynkSimpleEsp32.h>
 
-long sendInterval = 60000;  // Default interval (in milliseconds) at which to send telemetry to AWS
+// InfluxDB
+#define INFLUXDB_URL "http://192.168.68.109:8086"
+#define INFLUXDB_TOKEN "1K941SBKEARLoRWT_7ypQr2HPPywvkwIZOlyYXNapqyZDr4ooSVw3r6kDIPYrl-1rv3j30uBff7scztDyLEh5A=="
+#define INFLUXDB_ORG "my-org"
+#define INFLUXDB_BUCKET "Esp32plants_data"
 
-String THINGNAME = "";  // Will store the device's unique name (derived from MAC address)
+InfluxDBClient client(
+  INFLUXDB_URL,
+  INFLUXDB_ORG,
+  INFLUXDB_BUCKET,
+  INFLUXDB_TOKEN
+);
+Point sensor("plant_data");
+bool autoMode = true;  
+bool tempAlertSent  = false;
+bool waterAlertSent = false;
+bool humidAlertSent = false;
 
-// Initialize secure WiFi client and MQTT client with 1024 byte buffer for messagesmqttrulraws iotlamcognitomes
-WiFiClientSecure net = WiFiClientSecure();
-MQTTClient client = MQTTClient(1024);
-    
+void influxTask(void *parameter) {
+  SensorData data;
+
+  for (;;) {
+    if (xQueueReceive(influxQueue, &data, portMAX_DELAY)) {
+
+      sensor.clearFields();
+      sensor.addField("temperature", data.temperature);
+      sensor.addField("humidity", data.humidity);
+      sensor.addField("waterlevel", data.waterlevel);
+
+      if (!client.writePoint(sensor)) {
+        Serial.print("Influx write failed: ");
+        Serial.println(client.getLastErrorMessage());
+      } else {
+        Serial.println("Influx write OK");
+      }
+
+      yield();  // RTOS-friendly
+    }
+  }
+}
+
+BLYNK_WRITE(V0) {
+  if (!autoMode) {
+    int value = param.asInt();
+    digitalWrite(DC_Pin, value ? HIGH : LOW);
+    Serial.println(value ? "Pump ON (manual)" : "Pump OFF (manual)");
+  }
+}
+
+BLYNK_WRITE(V4) {
+  autoMode = param.asInt();
+  Serial.println(autoMode ? "AUTO mode" : "MANUAL mode");
+}
+
 float readLM75Temperature() {
+/*
   Wire.beginTransmission(LM75_ADDR);
   Wire.write(0x00);           // Temperature register
   Wire.endTransmission();
@@ -35,193 +84,155 @@ float readLM75Temperature() {
   }
 
   return NAN;
+  */
+  return random(0,40);
+}
+
+float readWaterlevel(){
+
+  //return map(analogRead(Anlog_Waterlevel_Pin), 0, 1023, 0,6);
+   float raw = analogRead(Anlog_Waterlevel_Pin);
+
+  // Map raw ADC to 0–4
+  float level = map(raw, WATER_EMPTY, WATER_FULL, 0, 4);
+
+  // Safety clamp
+  level = constrain(level, 0, 4);
+
+  return level;
 }
 
 float readH33PHumidity(){
-  
-  return map(analogRead(Anlog_Pin), 0, 1023, 0, 100);;
+  //return map(analogRead(Anlog_Pin), 0, 1023, 0, 100);;
+  int humidity=map(analogRead(Anlog_Pin), DRY_SOIL, WET_SOIL, 0, 100);
+  humidity = constrain(humidity, 0, 100);
+  return  humidity;
 }
-// Establishes connection to AWS IoT Core via WiFi and MQTT
-void connectAWS() {
-  // Set WiFi to station mode (client mode, not access point)
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-
-  // Get the MAC address to use as unique device identifier
-  THINGNAME = WiFi.macAddress();
-  // Remove colons from the MAC address string (AWS IoT thing names cannot contain colons)
-  for (int i = 0; i < THINGNAME.length(); i++) {
-    if (THINGNAME.charAt(i) == ':') {
-      THINGNAME.remove(i, 1);
-      i--;
-    }
-  }
-
-  Serial.println();
-  Serial.print("MAC Address: ");
-  Serial.println(THINGNAME);
+void connectBlynk() {
+  // Set WiFi to station mode (client me:\Ouyangjing\IOT24\examarbet\esp32s3\GPIO.hode, not access point)
+ // WiFi.mode(WIFI_STA);
+  //WiFi.begin(ssid, password);
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, password);
+  Blynk.syncVirtual(V0); 
+  Blynk.syncVirtual(V4); 
 
   Serial.println("Connecting to Wi-Fi");
+  // Optional: Print local IP
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
 
-  // Wait until WiFi connection is established
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
   }
 
-  // Configure WiFiClientSecure to use the AWS IoT device credentials for mutual TLS authentication
-  net.setCACert(AWS_CERT_CA);           // Root CA certificate
-  net.setCertificate(AWS_CERT_CRT);     // Device certificate
-  net.setPrivateKey(AWS_CERT_PRIVATE);  // Device private key
-
-  // Connect to the MQTT broker on the AWS endpoint (port 8883 is the standard MQTT over TLS port)
-  client.begin(AWS_IOT_ENDPOINT, 8883, net);
-
-  // Register callback function to handle incoming MQTT messages
-  client.onMessage(messageHandler);
-
-  Serial.print("Connecting to AWS IOT");
-
-  // Attempt to connect to AWS IoT using the thing name as client ID
-  while (!client.connect(THINGNAME.c_str())) {
-    Serial.print(".");
-    delay(100);
-  }
-
-  // Verify connection was successful
-  if (!client.connected()) {
-    Serial.println("AWS IoT Timeout!");
-    return;
-  }
-
-  // Subscribe to the downlink topic to receive commands from AWS
-  client.subscribe(THINGNAME + AWS_IOT_SUBSCRIBE_TOPIC);
-
-  Serial.println("AWS IoT Connected!");
-}
-void publishShadowState(float temperature, float humidity) {
-  JsonDocument doc;
-   
-  bool temperatureAlarm = temperature > temperature_threshold;
-  bool humidityAlarm = humidity > humidity_threshold;
-  doc["state"]["reported"]["temperature"] = temperature;
-  doc["state"]["reported"]["humidity"] = humidity;
-    doc["state"]["reported"]["temperatureAlarm"] = temperatureAlarm;
-  doc["state"]["reported"]["humidityAlarm"] = humidityAlarm;
-
-  char buffer[256];
-  serializeJson(doc, buffer);
-
-  Serial.print("Updating shadow: ");
-  Serial.println(buffer);
-
-  client.publish("$aws/things/" + THINGNAME + "/shadow/update", buffer);
-}
-
-
-
-// Configures AWS IoT Device Shadow subscriptions and requests current shadow state
-void setupShadow() {
-  // Subscribe to shadow topics to receive shadow updates
-  client.subscribe("$aws/things/" + THINGNAME + "/shadow/get/accepted");  // Receive accepted shadow get response
-  client.subscribe("$aws/things/" + THINGNAME + "/shadow/get/rejected");  // Receive rejected shadow get responsedev
-  //client.subscribe("$aws/things/" + THINGNAME + "/shadow/update/accepted");  // Optional: receive update confirmations
-  client.subscribe("$aws/things/" + THINGNAME + "/shadow/update/delta");   // Receive delta updates (when desired state changes)
-
-  // Request the current device shadow state from AWS
-  client.publish("$aws/things/" + THINGNAME + "/shadow/get");
-}
-
-// Publishes telemetry data to AWS IoT Core
-// Returns true if publish was successful, false otherwise
-bool publishTelemetry(String payload) {
-  Serial.print("Publishing: ");
-  Serial.println(payload);
-  return client.publish(THINGNAME + AWS_IOT_PUBLISH_TOPIC, payload);
-}
-
-// Callback function that handles incoming MQTT messages
-void messageHandler(String &topic, String &payload) {
-  Serial.println("incoming: " + topic + " - " + payload);
-
-  // Parse the JSON payload
-  JsonDocument doc;
-  deserializeJson(doc, payload);
-
-  // Handle device shadow messages
-  if (topic.endsWith("/shadow/get/accepted")) {
-    // Received accepted shadow get response - extract desired state
-    updateSettings(doc["state"]["desired"]);
-  } else if (topic.endsWith("/shadow/update/delta")) {
-    // Received shadow delta update - extract the changed state
-    updateSettings(doc["state"]);
-  }
-}
-
-// Updates device settings based on shadow document and reports back to AWS
-void updateSettings(JsonDocument settingsObj) {
-
-  Serial.println("Received DeviceShadow document: ");
-  serializeJson(settingsObj, Serial);
-  Serial.println();
-
-  // Update send interval if present in shadow document (convert seconds to milliseconds)
-  if (settingsObj.containsKey("sendIntervalSeconds")) {
-    sendInterval = settingsObj["sendIntervalSeconds"].as<int>() * 1000;
-    Serial.println("Send interval updated to: " + String(sendInterval / 1000) + " seconds");
-  }
-
-    if (settingsObj.containsKey("temperature_threshold")) {
-        temperature_threshold = settingsObj["temperature_threshold"].as<float>();
-    }
-    if (settingsObj.containsKey("humidity_threshold")) {
-        humidity_threshold = settingsObj["humidity_threshold"].as<float>();
-    }
-  // Create response document to report current state back to shadow
-  JsonDocument docResponse;
-  docResponse["state"]["reported"] = settingsObj;  // Report the received settings as current state
-  char jsonBuffer[512];
-  serializeJson(docResponse, jsonBuffer);
-
-  // Report back to device shadow to acknowledge the settings were applied
-  Serial.print("Sending reported state to AWS: ");
-  serializeJson(docResponse, Serial);
-  Serial.println();
-
-  client.publish("$aws/things/" + THINGNAME + "/shadow/update", jsonBuffer);
-}
-
-// Arduino setup function - runs once at startup
 void setup() {
-  pinMode(Anlog_Pin,INPUT_PULLUP);
-  Serial.begin(115200);  // Initialize serial communication for debugging
-  delay(2000);            // Wait for serial monitor to connect
+  Serial.begin(115200);
+  delay(1000);
 
-  Wire.begin(SDA,SCL);
-  connectAWS();           // Establish connection to AWS IoT Core
-  setupShadow();          // Configure and request device shadow
+  pinMode(Anlog_Pin, INPUT);
+  pinMode(Anlog_Waterlevel_Pin, INPUT);
+  pinMode(DC_Pin, OUTPUT);
+  pinMode(Fan_Pin, OUTPUT);
+  pinMode(LED_Pin, OUTPUT);
+
+  Wire.begin(SDA, SCL);
+  // ---- Blynk ----
+  connectBlynk();
+  // ---- InfluxDB ----
+  sensor.addTag("device", "esp32s3");
+
+  if (client.validateConnection()) {
+    Serial.println("InfluxDB connected");
+  } else {
+    Serial.println(client.getLastErrorMessage());
+  }
+
+  // ---- Queue ----
+  influxQueue = xQueueCreate(5, sizeof(SensorData));
+  if (influxQueue == NULL) {
+    Serial.println("Queue creation failed!");
+    while (1);
+  }
+
+  // ---- Task on Core 1 ----
+  xTaskCreatePinnedToCore(
+    influxTask,
+    "InfluxTask",
+    8192,
+    NULL,
+    1,
+    &influxTaskHandle,
+    1
+  );
+
+  Serial.println("System ready");
 }
 
 // Arduino main loop - runs continuously
 void loop() {
   // Use static variable to track last send time (initialized to allow immediate first send)
-  static unsigned long previousMillis = -sendInterval;
-  temperature= readLM75Temperature();
-  humidity=readH33PHumidity();
- 
-  // Check if it's time to send telemetry data
-  if (millis() - previousMillis >= sendInterval) {
-    // Update the last send time
-    previousMillis = millis();
+   Blynk.run();
+  
+  if (millis() - lastSensorRead >= SENSOR_INTERVAL) {
+    lastSensorRead = millis();
 
-    // Publish simulated sensor data (temperature and humidity) as JSON
-     bool sendResult = publishTelemetry("{\"temperature\":" + String(temperature, 2) + ",\"humidity\":" + String(humidity,2) + "}");
-      publishShadowState(temperature, humidity);
-    // Restart ESP32 if publish failed (indicates connection issue)
-    if (sendResult == 0)
-      ESP.restart();
+    // ---- Read sensors ----
+    temperature = readLM75Temperature();
+    humidity = readH33PHumidity();
+    waterlevel = readWaterlevel();
+
+
+    // ---------- ALARMS (AUTO + MANUAL) ----------
+   
+    // Temperature alarm
+    if (temperature > 28 && !tempAlertSent) {
+      Blynk.logEvent("high_temperature_alert", "Temperature too high");
+    
+      tempAlertSent = true;
+    }
+    if (temperature <= 28) tempAlertSent = false;
+
+    // Water level alarm
+    if (waterlevel <= 1 && !waterAlertSent) {
+      Blynk.logEvent("low_waterlevel_alert", "Water level too low");
+    
+      waterAlertSent = true;
+    }
+    if (waterlevel > 1) waterAlertSent = false;
+
+    // Humidity alarm
+    if (humidity < 40 && waterlevel > 1 && !humidAlertSent) {
+      Blynk.logEvent("low_humidity_alert", "Soil is too dry");
+     
+      humidAlertSent = true;
+    }
+    if (humidity >= 40) humidAlertSent = false;
+    bool alarmActive =
+    (temperature > 28) ||
+    (waterlevel <= 1) ||
+    (humidity < 40 && waterlevel > 1);
+
+    Blynk.virtualWrite(V5, alarmActive ? 255 : 0);
+
+    // ---- Automatic control ----
+    if (autoMode) {
+       digitalWrite(Fan_Pin, temperature > 28 ? HIGH : LOW);
+
+    if (humidity < 40 && waterlevel > 1) {
+        
+        digitalWrite(DC_Pin, HIGH);
+        digitalWrite(LED_Pin, HIGH);
+      } else {
+        digitalWrite(DC_Pin, LOW);
+        digitalWrite(LED_Pin, LOW);
+      }
+    }
+
+    // ---- Send to Blynk ----
+    Blynk.virtualWrite(V1, temperature);
+    Blynk.virtualWrite(V2, humidity);
+    Blynk.virtualWrite(V3, waterlevel);
+
+    // ---- Send to Influx queue ----
+    SensorData data = {temperature, humidity, waterlevel};
+    xQueueSend(influxQueue, &data, 0);
   }
-
-  // Process incoming MQTT messages and maintain connection
-  client.loop();
 }
